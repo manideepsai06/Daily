7e3b8aebe5b601a8a4133d20af33667678e74d04
manideepsai06 <118163216+manideepsai06@users.noreply.github.com>
2023-06-14 14:56:51 +0000

    14/06/2023

---
+++ Zed.java
@@ -0,0 +1,20 @@
+public class Zed {
+    public static void Star(){
+    for(int m=0;m<5;m++){
+    System.out.print("*");
+    }
+    for(int j=3;j>0;j--){
+        System.out.println();
+            for(int i=j;i>0;i--){
+               System.out.print(" ");
+                                }
+               System.out.print("* ");
+                       }  System.out.println();
+    for(int l=0;l<5;l++){
+    System.out.print("*");
+                        }               
+                             }
+ public static void main(String[] args) {
+    Zed.Star();  
+                                        }
+}
